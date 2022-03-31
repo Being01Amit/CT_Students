990665d50c1751c43e3d3f47c6a3232730a70916
amit <amitsunar54321@gmail.com>
2022-03-31 11:15:58 +0530

    0.0.3 "Adding Functionality to Google SignIn"

--- app/src/main/java/com/example/ctstudents/MainActivity.kt
+++ app/src/main/java/com/example/ctstudents/MainActivity.kt
@@ -1,11 +1,17 @@
-import com.example.ctstudents.R
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ctstudents.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
 }
--- app/src/main/java/com/example/ctstudents/SignActivity.kt
+++ app/src/main/java/com/example/ctstudents/SignActivity.kt
@@ -2,10 +2,28 @@ package com.example.ctstudents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
 
 class SignActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
+
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        var mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Set the dimensions of the sign-in button.
+        // Set the dimensions of the sign-in button.
+        val signInButton = findViewById<SignInButton>(R.id.sign_in_button)
+        signInButton.setSize(SignInButton.SIZE_STANDARD)
+
+
     }
 }
