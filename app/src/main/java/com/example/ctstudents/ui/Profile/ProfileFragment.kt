9cb580d9bf0409a9fd8ea12c7f3db5b454c65f4a
amit <amitsunar54321@gmail.com>
2022-06-10 16:36:48 +0530

    "Fragment done for now"

--- app/src/main/java/com/example/ctstudents/ui/Profile/ProfileFragment.kt
+++ app/src/main/java/com/example/ctstudents/ui/Profile/ProfileFragment.kt
@@ -1,5 +1,7 @@
 package com.example.ctstudents.ui.Profile
 
+import android.content.Intent
+import android.content.Intent.getIntent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +16,9 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val intent : Intent
+
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
