a93d969aef056405a7caa384848e97075d8a6d22
amit <amitsunar54321@gmail.com>
2022-06-10 16:35:55 +0530

    "Marks Kotin file is added"

---
+++ app/src/main/java/com/example/ctstudents/ui/Home/marksCal.kt
@@ -0,0 +1,12 @@
+package com.example.ctstudents.ui.Home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ctstudents.R
+
+class marksCal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_marks_cal)
+    }
+}
