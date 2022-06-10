29ba610a91b7dd355da176a90df15b436d7ef4cd
amit <amitsunar54321@gmail.com>
2022-06-10 16:33:37 +0530

    "Recycler view work done here"

--- app/src/main/java/com/example/ctstudents/ui/Feed/FeedFragment.kt
+++ app/src/main/java/com/example/ctstudents/ui/Feed/FeedFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
+import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.example.ctstudents.R
@@ -45,34 +46,29 @@ open class FeedFragment : Fragment(), NewsItemClick {
 
     private fun fetchData() {
         val url =
-            ""
+            "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=79a7ecb9953d4d858d3a280dcdf3f06f"
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, url, null,
-
-            { response ->
-                try {
-                    val NewsJsonArray = response.getJSONArray("articles")
-                    val newsArray = ArrayList<News>()
-                    for (i in 0 until NewsJsonArray.length()) {
-                        val newsJsonObject = NewsJsonArray.getJSONObject(i)
-                        val news = News(
-                            newsJsonObject.getString("author"),
-                            newsJsonObject.getString("title"),
-                            newsJsonObject.getString("description"),
-                            newsJsonObject.getString("url"),
-                            newsJsonObject.getString("urlToImage")
-                        )
-                        newsArray.add(news)
-                    }
-
-                    feedAdapter.updateNews(newsArray)
-
-                } catch (e: JSONException) {
-                    e.printStackTrace()
+            {
+                val NewsJsonArray = it.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for (i in 0 until NewsJsonArray.length()) {
+                    val newsJsonObject = NewsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("description"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
                 }
-            }) { error ->
-            Log.d("Amit", "error")
-        }
+                feedAdapter.updateNews(newsArray)
+
+            }, {
+                Log.d("VolleyError", "Something went wrong")
+            }
+        )
         MySingleton.getInstance(NewsFeed.context).addToRequestQueue(jsonObjectRequest)
     }
 
